b6fbe674c777ae5da6ae034d7491eb17f318554a
Johnny <johnnycalderondeveloper@gmail.com>
2020-09-28 19:20:44 +0200

    adding .env  creating mongoose connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const dbName = process.env.DB_NAME
+const dbPassword = process.env.DB_PASSWORD
+const dbUser = process.env.DB_USER
+
 const app = express();
 
+//connecting to mongoDB server
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.or615.mongodb.net/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 
 
 app.use('/graphql-f1', graphqlHTTP({
     schema,
-    graphiql:true
+    graphiql: true
 }))
 
 
