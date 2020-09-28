2a93adc6a2b44153596ac15f7f6bead0fb91f2cc
Johnny <johnnycalderondeveloper@gmail.com>
2020-09-28 20:08:22 +0200

    adding a schema for the mongodb collection

--- server/models/Circuit.js
+++ server/models/Circuit.js
@@ -4,5 +4,15 @@ const Schema = mongoose.Schema
 
 
 const circuitSchema = new Schema({
-    
-})
+    name: String,
+    city: String,
+    fastestLap: String,
+    fastestLapDriver: String,
+    length: String,
+    country: String,
+    fastestLapCar: String,
+    fastestLapYear: String,
+    fastestLapTeam: String,
+});
+
+module.exports = mongoose.model('Circuit', circuitSchema)
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,23 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+
+const Circuit = require('../models/Circuit')
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull,
 
 } = graphql
 //dummy data for
-
 const circuits = [
     { name: 'Spa-FrancoChamps', city: 'Stavelot', id: '1', fastestLap: "1:46.286", fastestLapDriver: "Valtterie Bottas", length: "7.004 km", country: "Belgium", fastestLapYear: "2018", fastestLapTeam: "Mercedes", fastestLapCar: "AMG F1 W09 EQ Power+" },
-    { name: 'Circuit de Monaco', city: 'Monte-Carlo', id: '2', fastestLap: "1:14.260", fastestLapDriver: "Max Verstappen", length: "3.340 km", country: "Monaco", fastestLapYear: "2018", fastestLapTeam: "Red Bull Racing", fastestLapCar: "RB14" },
+    { name: "Circuit de Monaco", city: 'Monte-Carlo', id: '2', fastestLap: "1:14.260", fastestLapDriver: "Max Verstappen", length: "3.340 km", country: "Monaco", fastestLapYear: "2018", fastestLapTeam: "Red Bull Racing", fastestLapCar: "RB14" },
     { name: 'Autódromo José Carlos Pace', city: 'São Paulo', id: '3', fastestLap: "1:10.540 ", fastestLapDriver: "Valtterie Bottas", length: "4.309 km", country: "Brazil", fastestLapYear: "2018", fastestLapTeam: "Mercedes", fastestLapCar: "AMG F1 W09 EQ Power+" },
-
 ]
+
 const CircuitType = new GraphQLObjectType({
     name: 'Circuit',
     fields: () => ({
@@ -54,8 +56,43 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 })
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addCircuit: {
+            type: CircuitType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                city: { type: new GraphQLNonNull(GraphQLString) },
+                fastestLap: { type: new GraphQLNonNull(GraphQLString) },
+                fastestLapDriver: { type: new GraphQLNonNull(GraphQLString) },
+                fastestLapYear: { type: new GraphQLNonNull(GraphQLString) },
+                fastestLapTeam: { type: new GraphQLNonNull(GraphQLString) },
+                fastestLapCar: { type: GraphQLString },
+                length: { type: new GraphQLNonNull(GraphQLString) },
+                country: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                let circuit = new Circuit({
+                    name: args.name,
+                    city: args.city,
+                    fastestLap: args.fastestLap,
+                    fastestLapDriver: args.fastestLapDriver,
+                    fastestLapYear: args.fastestLapYear,
+                    fastestLapTeam: args.fastestLapTeam,
+                    fastestLapCar: args.fastestLapCar,
+                    length: args.length,
+                    country: args.country,
+                });
+                return circuit.save()
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation:Mutation
 })
 // const TeamsType = new GraphQLObjectType({
 //     name: 'Teams',
